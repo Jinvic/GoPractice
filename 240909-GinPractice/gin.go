13d5c50b8ecd14552eea678bf4248840e38f2fb2
Jwb <jiangwenbin@readboy.com>
2024-09-09 15:14:04 +0800

    feat: 首页&240909.3 3. **模板渲染**

--- 240909-GinPractice/gin.go
+++ 240909-GinPractice/gin.go
@@ -3,12 +3,18 @@ package main
 import (
 	_ "fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	route := gin.Default()
+
+	route.LoadHTMLGlob("./templates/*")
+	route.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", nil)
+	})
 
 	// 1. **创建一个简单的 REST API**
 	route.GET("/ping", func(ctx *gin.Context) {
@@ -38,9 +44,17 @@ func main() {
 
 	// 2. **处理静态文件**
 	route.Static("/static", "./static")
-	route.LoadHTMLFiles("./templates/gin_doc.html")
+	// route.LoadHTMLFiles("./templates/gin_doc.html")
 	route.GET("/gin_doc", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "gin_doc.html", nil)
+	})
+
+	// 3. **模板渲染**
+	route.GET("/time", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "time.html", gin.H{
+			"title": "当前时间",
+			"time":  time.Now().String(),
+		})
 	})
 
 	route.Run(":8080")
